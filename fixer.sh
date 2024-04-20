85a4da72e56c7f9d6275eb856271ef2a51ef7b9e
Juan Rangel Díaz <zerohack16@hotmail.com>
2024-04-20 14:10:11 -0600

    Update fixer.sh
    color shit

--- fixer.sh
+++ fixer.sh
@@ -1,62 +1,80 @@
 #!/bin/bash
 # Descripción: mini script para la instalación de Let's encrypt en directadmin.
 #Author: Juan Rangel
+
+#bastionx rainbow
+BLACK=$(tput setaf 0)
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+YELLOW=$(tput setaf 3)
+LIME_YELLOW=$(tput setaf 190)
+POWDER_BLUE=$(tput setaf 153)
+BLUE=$(tput setaf 4)
+MAGENTA=$(tput setaf 5)
+CYAN=$(tput setaf 6)
+WHITE=$(tput setaf 7)
+BRIGHT=$(tput bold)
+NORMAL=$(tput sgr0)
+BLINK=$(tput blink)
+REVERSE=$(tput smso)
+UNDERLINE=$(tput smul)
+
 echo "++++++++++++++++++++++++++++++++++++"
-echo "Dfixer script"
+printf "${RED}"
 echo "++++++++++++++++++++++++++++++++++++"
 # it will fix Diego's lazy ass VPS image
 # Update Server Software
-echo "+++++++++ updating server software +++++++++ "
+printf "${RED}+++++++++ updating server software +++++++++ "
 /usr/local/cpanel/scripts/rpmup
 # System Update
-echo "+++++++++ Perfoming system update (this one takes time) +++++++++ "
+printf "${RED}+++++++++ Perfoming system update (this one takes time) +++++++++ "
 /usr/local/cpanel/scripts/sysup 
 # Upgrade cPanel
-echo "+++++++++ Updating cPanel +++++++++ "
+printf "${RED}+++++++++ Updating cPanel +++++++++ "
 /usr/local/cpanel/scripts/upcp
 # Revisa la versión de cPanel
-echo "+++++++++ Checking cPanel version +++++++++ "
+printf "${RED}+++++++++ Checking cPanel version +++++++++ "
 /usr/local/cpanel/cpanel -V 
-echo "+++++++++ removing troublesome packages for a clean start +++++++++ "
+printf "${RED}+++++++++ removing troublesome packages for a clean start +++++++++ "
 yum -y remove ea-php-cli-lsphp-1.0.0-11.16.2.cpanel.x86_64 ea-brotli-1.0.9-2.2.2.cpanel.x86_64 ea-php71-runtime-7.1.33-6.6.2.cpanel.x86_64 ea-php56-runtime-5.6.40-7.9.2.cpanel.x86_64 ea-php55-runtime-5.5.38-11.13.2.cpanel.x86_64 ea-php70-runtime-7.0.33-7.9.2.cpanel.x86_64 ea-libtidy-5.4.0-3.3.2.cpanel.x86_64
-echo "+++++++++ perfomning a yum-complete-transaction --cleanup-only +++++++++  "
+printf "${RED}+++++++++ perfomning a yum-complete-transaction --cleanup-only +++++++++  "
 yum install yum-utils
 yum-complete-transaction --cleanup-only
-echo "+++++++++ checking and fixing cpanel rpms +++++++++  "
+printf "${RED}+++++++++ checking and fixing cpanel rpms +++++++++  "
 scripts/check_cpanel_pkgs
-echo "+++++++++ rebuilding php fpm config +++++++++ "
+printf "${RED}+++++++++ rebuilding php fpm config +++++++++ "
 /scripts/php_fpm_config --rebuild
-echo "+++++++++ updating cpanel +++++++++ "
+printf "${RED}+++++++++ updating cpanel +++++++++ "
 /scripts/upcp 
 
-echo "+++++++++ overwriting csf config +++++++++ "
+printf "${RED}+++++++++ overwriting csf config +++++++++ "
 echo > /etc/csf/csf.conf
 gwet https://cucei.com.mx/fixer/csf
 cat csf > /etc/csf/csf.conf
-echo "+++++++++ restarting csf +++++++++ "
+printf "${RED}+++++++++ restarting csf +++++++++ "
 csf -r
 
-echo "+++++++++ settin up port 3500 on SSH +++++++++ "
+printf "${RED}+++++++++ settin up port 3500 on SSH +++++++++ "
 sed -i 's/#Port 22/Port 3500/g' /etc/ssh/sshd_config;
 sed -i 's/Port 22/Port 3500/g' /etc/ssh/sshd_config;
-echo "+++++++++ restarting ssh +++++++++ "
+printf "${RED}+++++++++ restarting ssh +++++++++ "
 service sshd restart
-echo "+++++++++ do not forget to try it out! +++++++++ "
+printf "${RED}+++++++++ do not forget to try it out! +++++++++ "
 
 
-echo "+++++++++ applying the new ea4 profile +++++++++ "
+printf "${RED}+++++++++ applying the new ea4 profile +++++++++ "
 wget https://cucei.com.mx/fixer/vpsnbx.json
 # Crea el nuevo profile de EA4
 touch /etc/cpanel/ea4/profiles/custom/vpsnbx.json
 cat vpsnbx.json > /etc/cpanel/ea4/profiles/custom/vpsnbx.json
 # Revisa el nuevo profile de EA4
 /usr/local/cpanel/3rdparty/perl/530/bin/json_pp -t null < /etc/cpanel/ea4/profiles/custom/vpsnbx.json
-echo "+++++++++ installing! +++++++++ "
+printf "${RED}+++++++++ installing! +++++++++ "
 # Instala profile en EA4
 /usr/local/bin/ea_install_profile --install /etc/cpanel/ea4/profiles/custom/vpsnbx.json
 
-echo "+++++++++ limpieza de yum +++++++++ "
+printf "${RED}+++++++++ limpieza de yum +++++++++ "
 yum clean all
 
-echo "+++++++++ upgrade final +++++++++ "
+printf "${RED}+++++++++ upgrade final +++++++++ "
 yum upgrade
